6e6c67608681c8f071446170dc4147d93292aa85
Mayank Srivastava <MayankSri@hcl.com>
2019-07-24 12:44:44 +0530

    Code Changes for QR reader on IOS

--- src/components/ScanProduct.js
+++ src/components/ScanProduct.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import QrReader from "react-qr-scanner";
+import { default as QrReader } from "react-qr-reader";
 import ItemsList from "./ItemsList";
 
 class ScanProduct extends Component {
@@ -10,10 +10,19 @@ class ScanProduct extends Component {
       result: null,
       items: [],
       cameraFlip: false,
-      cameraOption: "rear"
+      cameraOption: "environment",
+      userAgent: "all"
     };
 
     this.handleScan = this.handleScan.bind(this);
+  }
+  componentDidMount = () => {
+    if (navigator.userAgent.indexOf("Safari") != -1 && (navigator.platform.indexOf("iPhone") != -1 || navigator.platform.indexOf("iPad") != -1 || navigator.platform.indexOf("iPod") != -1)) {
+      this.setState({ userAgent: "safari" });
+    }
+    else {
+      this.setState({ userAgent: "all" });
+    }
   }
 
   handleScan(data) {
@@ -27,12 +36,13 @@ class ScanProduct extends Component {
     console.error(err);
   }
   handleCameraFlip = e => {
+    console.log("Flip Started", this.state.cameraFlip);
     e.preventDefault();
     this.setState({
       cameraFlip: !this.state.cameraFlip
     });
     this.setState({
-      cameraOption: this.state.cameraFlip ? "front" : "rear"
+      cameraOption: this.state.cameraFlip ? "user" : "environment"
     });
   };
 
@@ -52,14 +62,14 @@ class ScanProduct extends Component {
                     facingMode={this.state.cameraOption}
                   />
                 </div>
-                <div className="d-flex align-items-center">
+                {this.state.userAgent === "all" && <div className="d-flex align-items-center">
                   <button
                     className="btn btn-primary btn-lg inherit-clr"
                     onClick={e => this.handleCameraFlip(e)}
                   >
                     <i className="fa fa-camera" aria-hidden="true" />
                   </button>
-                </div>
+                </div>}
               </div>
             )}
 
